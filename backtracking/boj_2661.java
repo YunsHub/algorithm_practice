df15f891a7eadc9f2e9ea4a07675521fc24b264d
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2022-11-01 07:39:51 +0900

    Update boj_2661.java

--- backtracking/boj_2661.java
+++ backtracking/boj_2661.java
@@ -12,21 +12,6 @@ public class boj_2661 {
 		N = Integer.parseInt(br.readLine());
 		result = "";
 		success = false;
-//		if (N == 1) {
-//			sb.append("1");
-//		} else if (N == 2) {
-//			sb.append("12");
-//		} else if (N == 3) {
-//			sb.append("121");
-//		} else if (N == 4) {
-//			sb.append("1213");
-//		} else if (N == 5) {
-//			sb.append("12131");
-//		} else if (N == 6) {
-//			sb.append("121312");
-//		} else if (N == 7) {
-//			sb.append("1213121");
-//		}
 
 		recur(0, result);
 
@@ -34,29 +19,6 @@ public class boj_2661 {
 		System.out.println(sb);
 	}
 
-//	public static void backtracking(int cur, String num) {
-//		if (result.length() == N) {
-//			result = num;
-//			return;
-//		}
-//
-//		for (int i = 1; i <= 3; i++) {
-//			num += i;
-//			for (int j = 1; j >= num.length() / 2; j++) {
-//				if (num.substring(num.length() - j) == num.substring((num.length() - (2 * j)), (num.length() - j))) {
-//					continue;
-//				}
-//			}
-//
-////			recur(cur, num + i);
-////
-////			if (num.charAt(cur) == Integer.toString(i).charAt(0)) {
-////				continue;
-////			}
-//
-//		}
-//
-//	}
 
 	public static void recur(int cur, String num) {
 		if (success) {
