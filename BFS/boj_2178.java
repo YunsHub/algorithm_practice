83f8b24d44123ea36b39e8922aa9fcc7bc42462b
KimJungYun <wkdrns3918@gmail.com>
2022-12-13 18:34:53 +0900

    [백준]2178 - Java

---
+++ BFS/boj_2178.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_2178 {
+    static int N, M;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+
+        visited[0][0] = true;
+        bfs(0, 0);
+
+        System.out.println(arr[N - 1][M - 1]);
+
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+
+        while (!q.isEmpty()) {
+            int now[] = q.poll();
+            int nx = now[0];
+            int ny = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int ox = nx + dx[i];
+                int oy = ny + dy[i];
+
+                if (ox >= 0 && oy >= 0 && ox < N && oy < M) {
+                    if (!visited[ox][oy] && arr[ox][oy] != 0) {
+                        q.add(new int[]{ox, oy});
+                        arr[ox][oy] = arr[nx][ny] + 1;
+                        visited[ox][oy] = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
