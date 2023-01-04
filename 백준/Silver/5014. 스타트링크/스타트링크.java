30130ed3d387da83a7d2a6b6c456c9cec103bf59
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-04 16:51:00 +0900

    [Silver I] Title: 스타트링크, Time: 172 ms, Memory: 52572 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,9 +1,5 @@
-import com.sun.org.apache.xpath.internal.objects.XBoolean;
-import sun.awt.image.ImageWatched;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
