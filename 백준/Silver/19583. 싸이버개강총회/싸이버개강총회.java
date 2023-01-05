77106712693ba217df75979ec99ee3d88c463e19
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-05 22:18:50 +0900

    [Silver II] Title: 싸이버개강총회, Time: 588 ms, Memory: 80716 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -7,41 +7,42 @@ import java.util.StringTokenizer;
 public class Main {
 
 
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int S = stoi(st.nextToken());
-        int E = stoi(st.nextToken());
-        int Q = stoi(st.nextToken());
+        int[] S = stoi(st.nextToken());
+        int[] E = stoi(st.nextToken());
+        int[] Q = stoi(st.nextToken());
 
         Set<String> participants = new HashSet<>();
         int result = 0;
         String record;
         while ((record = br.readLine()) != null) {
+//            if(record.length() == 0){
+//                break;
+//            }
             st = new StringTokenizer(record);
-            int time = stoi(st.nextToken());
+            int[] time = stoi(st.nextToken());
             String name = st.nextToken();
 
-            if(time <= S){
+            if (time[0] < S[0] || (time[0] == S[0] && time[1] <= S[1])) {
                 participants.add(name);
-            }
-            else if(E <= time && time <= Q){
-                if(participants.contains(name)){
+            } else if ((time[0] > E[0] || (time[0] == E[0] && time[1] >= E[1]))
+                    && (time[0] < Q[0] || (time[0] == Q[0] && time[1] <= Q[1]))) {
+                if (participants.contains(name)) {
                     participants.remove(name);
                     result++;
                 }
             }
-//            System.out.println(result);
         }
 
         System.out.println(result);
 
     }
 
-    public static int stoi(String time){
+    public static int[] stoi(String time) {
         String[] temp = time.split(":");
-        return Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+        return new int[]{Integer.parseInt(temp[0]), Integer.parseInt(temp[1])};
     }
 }
