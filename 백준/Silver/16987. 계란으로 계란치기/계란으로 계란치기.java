1b53090a8d9e3a4726755833bb38db1ef5741dae
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-08 17:50:56 +0900

    [Silver I] Title: 계란으로 계란치기, Time: 176 ms, Memory: 13604 KB -BaekjoonHub

---
+++ 백준/Silver/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, eggs[][], crackedCnt;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        eggs = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            eggs[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        }
+
+        simulation(0, 0);
+        System.out.println(crackedCnt);
+    }
+
+    public static void simulation(int now, int cnt){
+        if(now == N){
+            crackedCnt = Math.max(crackedCnt, cnt);
+            return;
+        }
+
+        if(eggs[now][0] <= 0){//손에 든 계란이 깨졌을 때
+            simulation(now + 1, cnt);
+        }
+        else{//손에 든 계란이 안 깨졌을 때
+            boolean cracked = false;
+            for (int i=0; i<N; i++){
+                if(i == now){
+                    continue;
+                }
+                if(eggs[i][0] > 0) {
+                    cracked = true;
+                    eggs[i][0] -= eggs[now][1];
+                    eggs[now][0] -= eggs[i][1];
+
+                    simulation(now + 1, cnt + (eggs[i][0] <= 0 ? 1 : 0) + (eggs[now][0] <= 0 ? 1 : 0));
+
+//                    if(eggs[i][0] <= 0 && eggs[now][0] <= 0){
+//                            simulation(now + 1, cnt + 2);
+//                    }
+//                    else if(eggs[i][0] <= 0 || eggs[now][0] <= 0){
+//                        simulation(now + 1, cnt + 1);
+//                    }
+//                    else{
+//                        simulation(now + 1, cnt);
+//                    }
+                    eggs[now][0] += eggs[i][1];
+                    eggs[i][0] += eggs[now][1];
+                }
+            }
+
+            if(!cracked){
+                crackedCnt = Math.max(crackedCnt, N-1);
+            }
+        }
+
+
+    }
+
+}
