85253d55eb782af54bbf48260cc9060342016814
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-10 17:06:05 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 200 ms, Memory: 20864 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -0,0 +1,88 @@
+import sun.awt.image.ImageWatched;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, result;
+    static int[][] arr;
+    static boolean[][] visited;
+    static ArrayList<int[]> list;
+    static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
+    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
+        list = new ArrayList<>();
+        list.add(new int[] {N - 1, 0});
+        list.add(new int[] {N - 1, 1});
+        list.add(new int[] {N - 2, 0});
+        list.add(new int[] {N - 2, 1});
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+
+            simulation(d - 1 ,s);
+        }
+
+        result = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++){
+                result += arr[i][j];
+            }
+        }
+
+        System.out.println(result);
+    }
+    public static void simulation(int direction, int speed) {
+        visited = new boolean[N][N];
+        // 1. 모든 구름이 d방향으로 s칸 이동한다.
+        for(int i=0, size = list.size(); i<size; i++){
+            list.get(i)[0] = (N + list.get(i)[0] + dx[direction] * (speed%N)) % N;
+            list.get(i)[1] = (N + list.get(i)[1] + dy[direction] * (speed%N)) % N;
+            // 2.각 구름에서 비가 내려 구름이 있는 칸의 바구니에 저장된 물의 양이 1 증가한다.
+            arr[list.get(i)[0]][list.get(i)[1]] ++;
+            visited[list.get(i)[0]][list.get(i)[1]] = true;
+        }
+        // 4. 2에서 물이 증가한 칸 (r, c)에 물복사버그 마법을 시전한다. 물복사버그 마법을 사용하면, 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r, c)에 있는 바구니의 물이 양이 증가한다.
+        for(int[] item : list) {
+            for(int i = 1; i < 8; i+=2) {
+                int nx = item[0] + dx[i];
+                int ny = item[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N) {continue;}
+                if(arr[nx][ny] > 0) {
+                    arr[item[0]][item[1]]++;
+                }
+            }
+        }
+
+
+        // 3. 구름이 모두 사라진다.
+        list = new ArrayList<>();
+        // 5. 바구니에 저장된 물의 양이 2 이상인 모든 칸에 구름이 생기고, 물의 양이 2 줄어든다. 이때 구름이 생기는 칸은 3에서 구름이 사라진 칸이 아니어야 한다.
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(arr[i][j] >= 2 && !visited[i][j]) {
+                    arr[i][j] -= 2;
+                    list.add(new int[] {i, j});
+                }
+            }
+        }
+    }
+}
