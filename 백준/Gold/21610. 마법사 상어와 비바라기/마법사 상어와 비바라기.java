d860b63ef218e229cd92bffbc4442684d481c18d
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-10 17:25:55 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 144 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -7,8 +7,8 @@ import java.util.*;
 public class Main {
     static int N, M, result;
     static int[][] arr;
-    static boolean[][] visited;
-    static ArrayList<int[]> list;
+    static boolean[][] cloud;
+    //static ArrayList<int[]> list;
     static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
     static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
 
@@ -19,11 +19,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N][N];
-        list = new ArrayList<>();
-        list.add(new int[] {N - 1, 0});
-        list.add(new int[] {N - 1, 1});
-        list.add(new int[] {N - 2, 0});
-        list.add(new int[] {N - 2, 1});
+        cloud = new boolean[N][N];
+        cloud[N-1][0] = cloud[N-1][1] = cloud[N-2][0] = cloud[N-2][1] = true;
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,7 +34,7 @@ public class Main {
             int d = Integer.parseInt(st.nextToken());
             int s = Integer.parseInt(st.nextToken());
 
-            simulation(d - 1 ,s);
+            simulation(d - 1 ,s % N);
         }
 
         result = 0;
@@ -50,37 +47,47 @@ public class Main {
         System.out.println(result);
     }
     public static void simulation(int direction, int speed) {
-        visited = new boolean[N][N];
-        // 1. 모든 구름이 d방향으로 s칸 이동한다.
-        for(int i=0, size = list.size(); i<size; i++){
-            list.get(i)[0] = (N + list.get(i)[0] + dx[direction] * (speed%N)) % N;
-            list.get(i)[1] = (N + list.get(i)[1] + dy[direction] * (speed%N)) % N;
-            // 2.각 구름에서 비가 내려 구름이 있는 칸의 바구니에 저장된 물의 양이 1 증가한다.
-            arr[list.get(i)[0]][list.get(i)[1]] ++;
-            visited[list.get(i)[0]][list.get(i)[1]] = true;
-        }
-        // 4. 2에서 물이 증가한 칸 (r, c)에 물복사버그 마법을 시전한다. 물복사버그 마법을 사용하면, 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r, c)에 있는 바구니의 물이 양이 증가한다.
-        for(int[] item : list) {
-            for(int i = 1; i < 8; i+=2) {
-                int nx = item[0] + dx[i];
-                int ny = item[1] + dy[i];
 
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N) {continue;}
-                if(arr[nx][ny] > 0) {
-                    arr[item[0]][item[1]]++;
+        // 1. 모든 구름이 d방향으로 s칸 이동한다.
+        boolean next[][] = new boolean[N][N];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(cloud[i][j]){
+                    int nx = (N + i + dx[direction] * speed) % N;
+                    int ny = (N + j + dy[direction] * speed) % N;
+                    next[nx][ny] = true;
+                    arr[nx][ny] ++; // 2.각 구름에서 비가 내려 구름이 있는 칸의 바구니에 저장된 물의 양이 1 증가한다.
+                }
+            }
+        }
+        cloud = next;
+
+        // 4. 2에서 물이 증가한 칸 (r, c)에 물복사버그 마법을 시전한다. 물복사버그 마법을 사용하면, 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 (r, c)에 있는 바구니의 물이 양이 증가한다.
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(cloud[i][j]){
+                    for(int d = 1; d < 8; d+=2){
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+                        if(nx < 0 || ny < 0 || nx >= N || ny >= N) {continue;}
+                        if(arr[nx][ny] > 0) {
+                            arr[i][j]++;
+                        }
+                    }
                 }
             }
         }
 
-
         // 3. 구름이 모두 사라진다.
-        list = new ArrayList<>();
         // 5. 바구니에 저장된 물의 양이 2 이상인 모든 칸에 구름이 생기고, 물의 양이 2 줄어든다. 이때 구름이 생기는 칸은 3에서 구름이 사라진 칸이 아니어야 한다.
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                if(arr[i][j] >= 2 && !visited[i][j]) {
+                if(cloud[i][j]){
+                    cloud[i][j] = false;
+                }
+                else if(arr[i][j] >= 2) {
                     arr[i][j] -= 2;
-                    list.add(new int[] {i, j});
+                    cloud[i][j] = true;
                 }
             }
         }
