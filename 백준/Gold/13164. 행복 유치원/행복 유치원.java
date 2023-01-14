749f9875438f037ddd65d9172da384f3e6ff615b
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-14 21:50:30 +0900

    [Gold V] Title: 행복 유치원, Time: 508 ms, Memory: 63276 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-//    static int[] arr;
-    static PriorityQueue<Integer> pq;
+    static int[] arr;
+    static int[] gap;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,40 +15,25 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        pq = new PriorityQueue<>();
+
+        arr = new int[N];
+        gap = new int[N - 1];
 
         st = new StringTokenizer(br.readLine());
-        int n1 = Integer.parseInt(st.nextToken());
-        for(int i = 1; i < N; i++) {
-            int n2 = Integer.parseInt(st.nextToken());
-            pq.add(n2 - n1);
-            n1 = n2;
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        for(int i = 0; i < N - 1; i++) {
+            gap[i] = arr[i + 1] - arr[i];
+        }
+
+        Arrays.sort(gap);
         int result = 0;
-        for(int i = N-K; i>0; i--){
-            result += pq.poll();
+        for(int i = 0; i < N- K; i++){
+            result += gap[i];
         }
         System.out.println(result);
-//        pq = new PriorityQueue<>((o1, o2)-> o2[0] - o1[0]);//차이 내림차순
-//
-//        st = new StringTokenizer(br.readLine());
-//        for(int i = 0; i < N; i++) {
-//            arr[i] = Integer.parseInt(st.nextToken());
-//        }
-//
-//        int gap[] = new int[N-1];
-//        for(int i = 0; i < N-1; i++){
-//            gap[i] = arr[i+1] -  arr[i];
-//            pq.add(new int[]{gap[i], i});
-//        }
-//
-//        for(int i = 0; i < K; i++){
-//            int[] remove = pq.poll();
-//            gap[remove[1]] = -1;
-//        }
-//
-//        for(int i=0; )
 
     }
 }
