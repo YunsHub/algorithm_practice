607c5b91cf275744b9256018f00e52a7fd1b0400
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-19 19:24:24 +0900

    [Gold IV] Title: 타임머신, Time: 240 ms, Memory: 16832 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -18,8 +16,7 @@ public class Main {
 
     static int N, M;
     static final long INF = Long.MAX_VALUE;
-//    static Edge[] arr;
-    static ArrayList<Edge> arr;
+    static Edge[] arr;
     static long[] distance;
 
     public static void main(String[] args) throws Exception {
@@ -28,15 +25,9 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-//        arr = new Edge[M + 1];
-        arr = new ArrayList<>();
+        arr = new Edge[M];
         distance = new long[N + 1];
         Arrays.fill(distance, INF);
-
-        int[][] test = new int[N+1][N+1];
-        for(int i=1; i<=N; i++){
-            Arrays.fill(test[i], 999_999_999);
-        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -44,24 +35,14 @@ public class Main {
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
 
-            test[A][B] = Math.min(test[A][B], C);
-//            arr[i] = new Edge(A, B, C);
-        }
-
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                if(test[i][j] < 999_999_999){
-                    arr.add(new Edge(i, j, test[i][j]));
-                }
-            }
+            arr[i] = new Edge(A, B, C);
         }
 
         BellmanFord(1);
 
         StringBuilder sb = new StringBuilder();
-//        System.out.println(Arrays.toString(distance));
         for (int i = 2; i <= N; i++) {
-            if (distance[i] >= INF) {
+            if (distance[i] == INF) {
                 sb.append("-1\n");
             } else {
                 sb.append(distance[i]).append("\n");
@@ -74,17 +55,14 @@ public class Main {
         distance[start] = 0;
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0, size = arr.size(); j < size; j++) {
-                Edge edge = arr.get(j);
+            for (Edge edge : arr) {
                 if (distance[edge.start] != INF && distance[edge.end] > distance[edge.start] + edge.cost) {
                     distance[edge.end] = distance[edge.start] + edge.cost;
                 }
             }
         }
 
-        for(int i = 0, size = arr.size(); i < size; i++) {
-            Edge edge = arr.get(i);
-
+        for(Edge edge : arr) {
             if (distance[edge.start] != INF && distance[edge.end] > distance[edge.start] + edge.cost) {
                 System.out.println("-1");
                 System.exit(0);
