d5acc96aed9b722512985edf7541c23085b692c0
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-30 22:11:28 +0900

    [Silver I] Title: 후보 추천하기, Time: 88 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -13,37 +12,57 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
-        int[] student = new int[101];
-        LinkedList<Integer> album = new LinkedList<>();
+        int[] star = new int[101]; //추천 받은 횟수
+        int[] regtime = new int[101]; //게시된 시간
+        int[] picture = new int[N]; // 게시된 후보 번호
+        int size = 0;
 
+        Arrays.fill(regtime, 1001);
+        Arrays.fill(picture, 101);
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(!album.contains(num)){
-                if(album.size() == N){
-                    int min = student[album.get(0)], idx = 0;
-                    for(int j = 1; j < N; j++){
-                        int no = album.get(j);
-                        if(student[no] < min){
-                            min = student[no];
-                            idx = j;
-                        }
-                    }
-                    student[album.get(idx)] = 0;
-                    album.remove(idx);
+            boolean isExist = false;
+            for(int j = 0; j < size; j++){
+                if(picture[j] == num){
+                    isExist = true;
                 }
-                album.add(num);
             }
 
-            student[num]++;
+            if(!isExist){
+                if(size == N){
+                    int minTime = 1001, minValue = 1001, idx = 0;
+                    for(int j = 0; j < size; j++){
+                        if(star[picture[j]] > minValue){
+                            continue;
+                        }
+                        if(star[picture[j]] == minValue && regtime[picture[j]] > minTime){
+                            continue;
+                        }
+                        minValue = star[picture[j]];
+                        minTime = regtime[picture[j]];
+                        idx = j;
+                    }
+                    star[picture[idx]] = 0;
+                    picture[idx] = num;
+                    regtime[num] = i;
+                }
+                else{
+                    picture[size++] = num;
+                    regtime[num] = i;
+                }
+            }
+
+            star[num]++;
 
         }
 
-        Collections.sort(album);
+        Arrays.sort(picture);
 
         StringBuilder sb = new StringBuilder();
-        for(int no : album){
+        for(int no : picture){
+            if(no > 100){break;}
             sb.append(no).append(" ");
         }
         System.out.println(sb);
