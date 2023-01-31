ebe736d70d49ba139063f339be8be99b0a9487e2
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-01-31 17:18:06 +0900

    [Gold V] Title: 벽장문의 이동, Time: 80 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, M, result;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        String[] opened = br.readLine().split(" ");
+        int left = Integer.parseInt(opened[0]);
+        int right = Integer.parseInt(opened[1]);
+
+        M = Integer.parseInt(br.readLine());
+        arr = new int[M];
+        for(int i = 0; i < M; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        result = Integer.MAX_VALUE;
+        dfs(0, left, right, 0);
+        System.out.println(result);
+    }
+
+    public static void dfs(int idx, int l, int r, int sum){
+        if(idx == M){
+            result = Math.min(result, sum);
+            return;
+        }
+
+        if(l==arr[idx] || r == arr[idx]){
+            dfs(idx+1, l, r, sum);
+        }
+        else if(l < arr[idx] && arr[idx] < r){
+            dfs(idx+1, arr[idx], r, sum + arr[idx] - l);
+            dfs(idx+1, l, arr[idx], sum + r - arr[idx]);
+        }
+        else if(arr[idx] < l){
+            dfs(idx+1, arr[idx], r, sum + l - arr[idx]);
+        }
+        else {
+            dfs(idx+1, l, arr[idx], sum + arr[idx] - r);
+        }
+    }
+
+}
