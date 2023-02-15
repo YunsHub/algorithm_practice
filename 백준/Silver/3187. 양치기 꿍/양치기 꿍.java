8bceee8527251e889fde6f785db0f070dc7cb4db
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-02-15 20:16:17 +0900

    [Silver I] Title: 양치기 꿍, Time: 100 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Silver/3187. 양치기 꿍/양치기 꿍.java
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.java
@@ -6,14 +6,19 @@ public class Main {
     static int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
     static char map[][];
 
-    public static void dfs(int x, int y){
+    public static void dfs(int x, int y) {
 
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx<0 || ny<0 || nx>=N || ny>=M || map[nx][ny]=='#'){continue;}
-            if(map[nx][ny]=='v'){w++;}
-            else if(map[nx][ny] == 'k'){s++;}
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] == '#') {
+                continue;
+            }
+            if (map[nx][ny] == 'v') {
+                w++;
+            } else if (map[nx][ny] == 'k') {
+                s++;
+            }
             map[nx][ny] = '#';
             dfs(nx, ny);
         }
@@ -27,30 +32,32 @@ public class Main {
         map = new char[N][M];
         int wolf = 0, sheep = 0;
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             String s = br.readLine();
-            for(int j=0; j<M; j++){
+            for (int j = 0; j < M; j++) {
                 map[i][j] = s.charAt(j);
             }
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(map[i][j] != '#'){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] != '#') {
                     w = s = 0;
-                    if(map[i][j]=='v'){w++;}
-                    else if(map[i][j] == 'k'){s++;}
+                    if (map[i][j] == 'v') {
+                        w++;
+                    } else if (map[i][j] == 'k') {
+                        s++;
+                    }
                     map[i][j] = '#';
                     dfs(i, j);
-                    if(w < s){
+                    if (w < s) {
                         sheep += s;
-                    }
-                    else{
+                    } else {
                         wolf += w;
                     }
                 }
             }
         }
-        System.out.println(sheep+" "+wolf);
+        System.out.println(sheep + " " + wolf);
     }
 }
