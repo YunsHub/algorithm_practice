b91b0da422c93dc2bd6d7f82015cbaf10a82d6ab
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-02-25 23:47:51 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+//         int num1 = common[1] - common[0];
+//         int num2 = common[2] - common[1];
+//         int num3 = common[1] / common[0];
+        
+//         if(num2 == num1) {
+//             answer = common[common.length - 1] + num1;
+//         } else{
+//             answer = common[common.length - 1] * num3;
+//         }
+        if (common[2] - common[1] == common[1] - common[0]){
+          answer = common[common.length - 1] + (common[1] - common[0]);
+        } else {
+          answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        return answer;
+    }
+}
