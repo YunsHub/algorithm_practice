5f1e396c92f331fc7fb90e7b13f7c14e2c929169
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-03-23 01:42:41 +0900

    [Silver I] Title: 달력, Time: 92 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Silver/20207. 달력/달력.java
+++ 백준/Silver/20207. 달력/달력.java
@@ -6,35 +6,15 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayList<Schedule> list = new ArrayList<>();
     static int result = 0;
     static int[] calendar;
-
-    public static class Schedule implements Comparable<Schedule> {
-        int startDay;
-        int endDay;
-        boolean use;
-
-        Schedule(int startDay, int endDay) {
-            this.startDay = startDay;
-            this.endDay = endDay;
-        }
-
-        @Override
-        public int compareTo(Schedule schedule) {
-            if(this.endDay == schedule.endDay) {
-                return this.startDay - schedule.startDay;
-            }
-            return this.endDay - schedule.endDay;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         calendar = new int[367];
-
+        
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int sd = Integer.parseInt(st.nextToken());
@@ -42,11 +22,9 @@ public class Main {
             for (int j = sd; j <= ed; j++) {
                 calendar[j]++;
             }
-            list.add(new Schedule(sd, ed));
         }
-        Collections.sort(list);
 
-        boolean check = false;
+        boolean check;
         int column = 0;
         int row = 0;
         for(int i = 1; i <= 366; i++) {
@@ -63,31 +41,7 @@ public class Main {
                 row = Math.max(row, calendar[i]);
             }
         }
-
-        //laminate();
         System.out.println(result);
     }
 
-//    public static void laminate() {
-//        int minColumn = Integer.MAX_VALUE;
-//        int maxColumn = 0;
-//        int day = 0;
-//        for (Schedule schedule : list) {
-//            if (!schedule.use) {
-//                if (day == 0 || day + 1 >= schedule.startDay) {
-//                    minColumn = Math.min(minColumn, schedule.startDay);
-//                    maxColumn = Math.max(maxColumn, schedule.endDay);
-//                    day = schedule.endDay;
-//                    schedule.use = true;
-//                } else {
-//                    laminate();
-//                }
-//            }
-//        }
-//        int row = 0;
-//        for (int i = minColumn; i <= maxColumn; i++) {
-//            row = Math.max(row, calendar[i]);
-//        }
-//        result += row * (maxColumn - minColumn + 1);
-//    }
 }
