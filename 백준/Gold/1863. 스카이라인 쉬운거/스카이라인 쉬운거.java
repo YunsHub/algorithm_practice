043185aa2300712d3e1543585f5775c6e044d185
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-03-25 00:23:22 +0900

    [Gold V] Title: 스카이라인 쉬운거, Time: 172 ms, Memory: 17228 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        int[] height = new int[N + 1];
+        Stack<Integer> building = new Stack<>();
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x= Integer.parseInt(st.nextToken());
+            int y= Integer.parseInt(st.nextToken());
+            height[i] = y;
+        }
+
+
+        for(int i = 0; i <= N; i++) {
+            while (!building.isEmpty() && building.peek() > height[i]) {
+                result++;
+                building.pop();
+            }
+            if (!building.isEmpty() && building.peek() == height[i]) {
+                continue;
+            }
+            building.add(height[i]);
+        }
+
+        System.out.println(result);
+
+    }
+}
