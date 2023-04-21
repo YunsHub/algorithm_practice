128c08c019f5c6e5c189f7e39070bf741f3bed84
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-04-21 10:56:22 +0900

    [Silver V] Title: 영화감독 숌, Time: 356 ms, Memory: 158620 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -5,23 +5,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        String num = "666";
-        while (true) {
-            if(num.contains("666")){
+        int cnt = 1;
+        int num = 666;
+        while (N != cnt) {
+            num++;
+            if(String.valueOf(num).contains("666")){
                 cnt++;
             }
 
-            if(N == cnt) {
-                System.out.println(num);
-                break;
-            }
-
-            int a = Integer.parseInt(num);
-            a++;
-            num = String.valueOf(a);
-
-
         }
+        System.out.println(num);
     }
 }
