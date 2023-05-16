c40e11f6baa8973837f7851ea1d8ca2844f3d2a0
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-05-16 16:12:03 +0900

    [Silver IV] Title: 가로수, Time: 404 ms, Memory: 21784 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        for(int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+        int width = Integer.MAX_VALUE;
+        int dis1 = num[1] - num[0];
+        for(int i = 1; i < N - 1; i++) {
+            int dis2 = num[i + 1] - num[i];
+            width = Math.min(gcd(dis1, dis2), width);
+            dis1 = dis2;
+        }
+        int cnt = 0;
+        int jump = num[0];
+        for(int i = 0; i < N; i++) {
+            while (true) {
+                if(num[i] == jump) {
+                    jump += width;
+                    break;
+                } else {
+                    jump += width;
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+    public static int gcd(int a, int b) {
+        if(a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+}
