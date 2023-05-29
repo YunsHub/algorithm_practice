aee0c44827520b4a7ef0f11dc82811019152c11e
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-05-29 19:16:01 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 96 ms, Memory: 12452 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -10,13 +10,12 @@ public class Main {
         recur(0, "");
     }
     public static void recur(int cnt, String str) {
+        System.out.println(str + "\"재귀함수가 뭔가요?\"");
         if(cnt == N) {
-            System.out.println(str + "\"재귀함수가 뭔가요?\"");
             System.out.println(str + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
             System.out.println(str + "라고 답변하였지.");
             return;
         }
-        System.out.println(str + "\"재귀함수가 뭔가요?\"");
         System.out.println(str + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
         System.out.println(str + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
         System.out.println(str + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
