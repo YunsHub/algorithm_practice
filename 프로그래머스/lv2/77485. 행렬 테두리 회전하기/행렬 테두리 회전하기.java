30db696039d27ca813b08dc5dfec7637fa270e0d
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-07-17 23:43:59 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 19.92 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,11 +1,9 @@
 import java.util.*;
 class Solution {
     public static int[][] map;
-    public static int[][] base;
     public static int min;
     public int[] solution(int rows, int columns, int[][] queries) {
         map = new int[rows][columns];
-        base = new int[rows][columns];
         int num = 1;
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
@@ -15,52 +13,43 @@ class Solution {
         int[] answer = new int[queries.length];
         int idx = 0;
         for(int i = 0; i < queries.length; i++) {
-            copyArray();
-            min = Integer.MAX_VALUE;
             int minX = queries[i][0] - 1;
             int minY = queries[i][1] - 1;
             int maxX = queries[i][2] - 1;
             int maxY = queries[i][3] - 1;
-            rightArrow(minX, minY + 1, maxY);
-            downArrow(minX + 1, maxY, maxX);
-            leftArrow(maxX, maxY - 1, minY);
-            topArrow(maxX - 1, minY, minX);
-            answer[idx++] = min;
+            
+            answer[idx++] = rotate(minX, minY, maxX, maxY);
         }
         
         
         return answer;
     }
-    public void rightArrow(int x, int y, int newY) {
-        for(int i = y; i <= newY; i++) {
-            map[x][i] = base[x][i - 1];
-            min = Integer.min(min, map[x][i]);
-        }
-    }
-    public void downArrow(int x, int y, int newX) {
-        for(int i = x; i <= newX; i++) {
-            map[i][y] = base[i - 1][y];
-            min = Integer.min(min, map[i][y]);
-        }
-    }
-    public void leftArrow(int x, int y, int newY) {
-        for(int i = y; i >= newY; i--) {
-            map[x][i] = base[x][i + 1];
-            min = Integer.min(min, map[x][i]);
-        }
-    }
-    public void topArrow(int x, int y, int newX) {
-        for(int i = x; i >= newX; i--) {
-            map[i][y] = base[i + 1][y];
-            min = Integer.min(min, map[i][y]);
-        }
-    }
     
-    public static void copyArray() {
-        for(int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++){
-                base[i][j] = map[i][j];
-            }
+    public int rotate(int minX, int minY, int maxX, int maxY) {
+        int temp = map[minX][maxY];
+        min = temp;
+        for(int i = maxY; i > minY; i--) {
+            map[minX][i] = map[minX][i - 1];
+            min = Integer.min(min, map[minX][i]);
         }
+        
+        for(int i = minX; i < maxX; i++) {
+            map[i][minY] = map[i + 1][minY];
+            min = Integer.min(min, map[i][minY]);
+        }
+        
+        for(int i = minY; i < maxY; i++) {
+            map[maxX][i] = map[maxX][i + 1];
+            min = Integer.min(min, map[maxX][i]);
+        }
+        
+        for(int i = maxX; i > minX + 1; i--) {
+            map[i][maxY] = map[i - 1][maxY];
+            min = Integer.min(min, map[i][maxY]);
+        }
+        map[minX + 1][maxY] = temp;
+        
+        return min;
     }
 }
+ 
