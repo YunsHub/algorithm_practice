e73105e809416ac2b47fc4cf7e1c31241626f65e
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-07-20 03:28:47 +0900

    [level 2] Title: 더 맵게, Time: 1687.91 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -6,21 +6,14 @@ class Solution {
         for(int foodScoville : scoville) {
             scovillePriorityQueue.add(foodScoville);
         }
-        while(!scovillePriorityQueue.isEmpty() && scovillePriorityQueue.peek() < K) {
-            int lowScoville1 = scovillePriorityQueue.poll();
-            if(!scovillePriorityQueue.isEmpty()) {
-                int lowScoville2 = scovillePriorityQueue.poll();
-                scovillePriorityQueue.add(lowScoville1 + (lowScoville2 * 2));
-                answer++;
-                continue;
-            }
-            if(lowScoville1 < K) {
-                answer = -1;
-                break;
-            }
-            
+        while(scovillePriorityQueue.size() > 1 && scovillePriorityQueue.peek() < K) {
+            scovillePriorityQueue.add(scovillePriorityQueue.poll()
+                                      + (scovillePriorityQueue.poll() * 2));
+            answer++;            
         }    
-        
+        if(scovillePriorityQueue.poll() < K) {
+            answer = -1;
+        }
         return answer;
     }
 }
