68afc2c8bd5126b90272864d3146ca0d13b7b5a8
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-08-20 23:26:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.47 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -18,17 +18,21 @@ class Solution {
         }
     }
     public int[] solution(String[] genres, int[] plays) {
+        // 장르별 총 plays를 구하기 위한 map 선언 후 삽입
         HashMap<String, Integer> stream = new HashMap<>();
         for(int index = 0; index < genres.length; index++) {
             stream.put(genres[index], stream.getOrDefault(genres[index], 0) + plays[index]);
         }
         
+        // 정렬을 위해 ArrayList선언
         ArrayList<Music> musicList = new ArrayList<>();
         for(String key : stream.keySet()) {
             musicList.add(new Music(key, stream.get(key)));        
         }
         Collections.sort(musicList);
         
+        // 총 play 수가 많은 순부터 같은 장르가 있으면 Arraylist에 넣고 정렬 후 
+        // 0,1에 해당하는 인덱스 삽입 (단 size가 1일 경우엔 0번 인덱스만 삽입)
         ArrayList<Integer> albumNum = new ArrayList<>();
         for(Music music : musicList) {
             ArrayList<Music> musicPlay = new ArrayList<>();
