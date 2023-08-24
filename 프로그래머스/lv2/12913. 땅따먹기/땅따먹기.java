0f20001ce24573de66ec725223fa63a5c4179c92
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-08-24 22:04:24 +0900

    [level 2] Title: 땅따먹기, Time: 12.19 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        for(int row = 1; row < land.length; row++) {
+            for(int col = 0; col < 4; col++) {
+                land[row][col] += Math.max(Math.max(land[row - 1][(col + 1) % 4], 
+                                                    land[row - 1][(col + 2) % 4]), 
+                                           land[row - 1][(col + 3) % 4]);
+            }
+        }
+        
+        
+        int answer = 0;
+        for(int index = 0; index < 4; index++) {
+            answer = Math.max(answer, land[land.length - 1][index]);
+        }
+        return answer;
+    }
+}
