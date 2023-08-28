e259fecd658e740408eb4207315b17d6defe89f6
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-08-28 23:51:18 +0900

    [level 2] Title: 주식가격, Time: 27.87 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,20 +2,27 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-         for(int i=0;i<prices.length-1;i++){
-            for(int j=i+1;j<prices.length;j++) {
-                answer[i]++;
-                if (prices[i] > prices[j])
-                    break;
-            }
-        }
-        // for(int index = 0; index <prices.length; index++) {
+        // for(int index = 0; index <prices.length - 1; index++) {
         //     for(int num = index + 1; num <prices.length; num++) {
-        //         if(prices[index] > prices[num]) break;
-        //         answer[index]++;                
+        //         answer[index]++;
+        //         if(prices[index] > prices[num]) break;    
         //     }
         // } 
         
+        Stack<Integer> stack = new Stack<>();
+        for(int index = 0; index < prices.length; index++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[index]) {
+                answer[stack.peek()] = index - stack.peek();
+                stack.pop();
+            }
+            stack.push(index);
+        }
+        
+        
+        while(!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - 1 - stack.peek();
+            stack.pop();
+        }
         
         return answer;
     }
