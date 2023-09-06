8fc4d31ce7da68faeca345e8fc27398fbc41f99c
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-09-06 14:31:59 +0900

    [unrated] Title: 미로 탈출, Time: 0.41 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.java
@@ -1,103 +1,73 @@
 import java.util.*;
 class Solution {
-    static int[][] arr;
-    static boolean[][] visited;
-    static boolean check = false;
-    static boolean check2 = false;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    public static class Node {
-        int x;
-        int y;
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
+    public int[] dx = {1, 0, -1, 0};
+    public int[] dy = {0, -1, 0, 1};
     public int solution(String[] maps) {
-        int answer = 0;
-        arr = new int[maps.length][maps[0].length()];    
-        
-        int x = 0;
-        int y = 0;
-        int exitX = 0;
-        int exitY = 0;
-        int leberX = 0;
-        int leberY = 0;
-        for(int i = 0; i < maps.length; i++) {
-            for(int j = 0; j < maps[i].length(); j++) {
-                if(maps[i].charAt(j)=='S') {
-                    x = i;
-                    y = j;
-                }
-                else if(maps[i].charAt(j)=='E') {
-                    exitX = i;
-                    exitY = j;
-                }
-                else if(maps[i].charAt(j)=='L') {
-                    leberX = i;
-                    leberY = j;
-                }
-            }
-        }
-        
-        visited = new boolean[maps.length][maps[0].length()];
-        bfs(x, y, maps, 1);
-        if(check) {
-            visited = new boolean[maps.length][maps[0].length()];    
-            bfs(leberX, leberY, maps, arr[leberX][leberY] + 1);
-            if(check2) {
-                answer = arr[exitX][exitY];    
-            } else{
-                answer = -1;
-            }
-        } else{
-            answer = -1;
-        }
+        //boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        int sx = 0;
+        int sy = 0;
+        int ex = 0;
+        int ey = 0;
         for(int i = 0; i < maps.length; i++) {
             for(int j = 0; j < maps[0].length(); j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+                 if(maps[i].charAt(j) == 'S') {
+                    sx = i;
+                    sy = j;
+                } else if(maps[i].charAt(j) == 'E') {
+                    ex = i;
+                    ey = j;
+                } 
+            }            
         }
+        //dfs(0, maps, visited, x, y);        
         
-        
-        
-        
-        return answer;
+        return bfs(maps, sx, sy, ex, ey);
     }
     
-    public static void bfs(int x, int y, String[] maps, int dis) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y));
-        visited[x][y] = true;
-        int result = dis;
-        while(!q.isEmpty()) {    
-            for(int i = 0, size = q.size(); i < size; i++) {
-                Node node = q.poll();
-                for(int j = 0; j < 4; j++) {
-                    int nx = node.x + dx[j];
-                    int ny = node.y + dy[j];
-                    if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length() || 
-                       maps[nx].charAt(ny) == 'X' || visited[nx][ny]) continue;                    
-                    
-                    arr[nx][ny] = result;  
-                    if(maps[nx].charAt(ny) == 'L') {
-                        check = true;
-                        return;
-                    }
-                    if(maps[nx].charAt(ny) == 'E' && check) {
-                        check2 = true;
-                        return;
-                    }
-                    q.add(new Node(nx, ny));
-                    visited[nx][ny] = true;
-                }    
+    public int bfs(String[] maps, int sx, int sy, int ex, int ey) {
+        int[][] map = new int[maps.length][maps[0].length()];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {sx, sy});
+        boolean result = false;
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = now[0] + dx[dir];
+                int ny = now[1] + dy[dir];
+                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length() || 
+                  map[nx][ny] != 0 || maps[nx].charAt(ny) == 'X') continue;
+                if(maps[nx].charAt(ny) == 'L') {    
+                    result = true;
+                    int dis = map[now[0]][now[1]] + 1;
+                    map = new int[maps.length][maps[0].length()];
+                    map[nx][ny] = dis;
+                    q.clear();
+                    q.add(new int[] {nx, ny});
+                    break;
+                } else {
+                    map[nx][ny] = map[now[0]][now[1]] + 1; 
+                    q.add(new int[] {nx, ny});
+                }
                 
             }
-            result++; 
-        }        
+        }
+        System.out.println(Arrays.deepToString(map));
+        if(map[ex][ey] == 0 || !result) return -1;
+        else return map[ex][ey];
     }
     
+    // public int dfs(int depth, String[] maps, boolean[][] visited, int x, int y) {
+    //     System.out.println(depth  +" " + x + " " + y);
+    //     visited[x][y] = true;
+    //     for(int dir = 0; dir < 4; dir++) {
+    //         int nx = x + dx[dir];
+    //         int ny = y + dy[dir];
+    //         if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps.length ||
+    //           visited[nx][ny] || maps[nx].charAt(ny) == 'X') continue;        
+    //         if(maps[nx].charAt(ny) == 'L') {
+    //             visited = new boolean[maps.length][maps[0].length()];
+    //         }
+    //         dfs(depth + 1, maps, visited, nx, ny);
+    //     }
+    // }
 }
