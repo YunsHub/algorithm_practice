dd346920b424f8b9eced2dcf1c8a4908ab9070e7
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-09-23 00:42:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.19 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,19 +1,39 @@
+import java.util.*;
 class Solution {
     public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-        
-        for(int i = 0; i < number.length() - k; i++) {
-            int max = 0;
-            for(int j = index; j < k + i + 1 ; j++) {
-                if(max < number.charAt(j) - '0') {
-                    max = number.charAt(j) - '0';
-                    index = j + 1;
-                }
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < number.length(); i++) {
+            char num = number.charAt(i);
+            while(!stack.isEmpty() && stack.peek() < num && k-- > 0) {
+                stack.pop();
             }
-            sb.append(max);
+            stack.push(num);
+            
         }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+        // while(!stack.isEmpty()) {
+        //     sb.insert(0, stack.pop());
+        // }
+        // return sb.toString();
+//         StringBuilder sb = new StringBuilder();
+//         int index = 0;
         
-        return sb.toString();
+//         for(int i = 0; i < number.length() - k; i++) {
+//             int max = 0;
+//             for(int j = index; j < k + i + 1 ; j++) {
+//                 if(max < number.charAt(j) - '0') {
+//                     max = number.charAt(j) - '0';
+//                     index = j + 1;
+//                 }
+//             }
+//             sb.append(max);
+//         }
+        
+//         return sb.toString();
     }
 }
