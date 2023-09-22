512d09a2c0e47cff5c04867bdd0518aa193be53d
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-09-23 05:32:27 +0900

    [level 3] Title: 단속카메라, Time: 22.40 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0;
         Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
         System.out.println(Arrays.deepToString(routes));
-        int last = routes[0][1];
+        int last = -30001;
         for(int[] route : routes) {
             if(route[0] > last) {
                 answer++;
