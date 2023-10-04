ea797322f40f174af8268ee242d47e284d4ebc6d
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-10-05 06:42:52 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 2.60 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,40 +1,41 @@
 import java.util.*;
-import java.text.*;
+import java.time.*;
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) throws ParseException {
-        ArrayList<Integer> result = new ArrayList<>();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-        Date todayDate = format.parse(today);    
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> list = new ArrayList<>();
+        HashMap<String, Long> termsMap = new HashMap<>();
+        for(String term : terms) {
+            String[] str = term.split(" ");
+            String type = str[0];
+            String month = str[1];
+            termsMap.put(type, Long.parseLong(month));
+        }
+        
+        String[] todatStr = today.split("\\.");  
+        int year = Integer.parseInt(todatStr[0]);
+        int month = Integer.parseInt(todatStr[1]);
+        int day = Integer.parseInt(todatStr[2]);
+        LocalDate now = LocalDate.of(year, month, day);
+        
         for(int i = 0; i < privacies.length; i++) {
-            StringTokenizer st1 = new StringTokenizer(privacies[i]);
-            String date = st1.nextToken();
-            String type = st1.nextToken();
-            int destroyDay = 0;
-            for(int j = 0; j < terms.length; j++) {
-                StringTokenizer st2 = new StringTokenizer(terms[j]);                
-                if(type.equals(st2.nextToken())) {
-                    destroyDay = Integer.parseInt(st2.nextToken());
-                    break;
-                }
-            }
-            Date calDate = format.parse(date);
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(calDate);
-            cal.add(Calendar.MONTH, destroyDay);
-            if(todayDate.after(format.parse(format.format(cal.getTime())))
-              || todayDate.equals(format.parse(format.format(cal.getTime())))) {
-                result.add(i + 1);
-            }
+            String[] str = privacies[i].split(" ");
+            String[] dateStr = str[0].split("\\.");
+            String type = str[1];
+            long term = termsMap.get(str[1]);
+            int y = Integer.parseInt(dateStr[0]);
+            int m = Integer.parseInt(dateStr[1]);
+            int d = Integer.parseInt(dateStr[2]);
+            LocalDate date = LocalDate.of(y, m, d);
+            LocalDate local = date.plusMonths(term);
             
+            if(!now.isBefore(local)) list.add(i + 1);
         }
         
-        int[] answer = new int[result.size()];
-        for(int i = 0 ; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
         
-    
-        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }        
         return answer;
     }
 }
