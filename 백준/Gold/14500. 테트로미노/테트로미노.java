eb30341c47d9b424589479634e36642c83230652
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2023-12-01 00:00:12 +0900

    [Gold IV] Title: 테트로미노, Time: 864 ms, Memory: 302648 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int answer = 0;
+    public static int[][] map;
+    public static boolean[][] visited;
+    public static int n,m;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                dfs(1, i, j ,map[i][j]);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void dfs(int depth, int x, int y, int sum) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        if(depth == 4) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        visited[x][y] = true;
+
+        for(int dir=0; dir<4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if(!isRange(nx, ny)) continue;
+            if(visited[nx][ny]) continue;
+
+            if(depth == 2) {
+                visited[nx][ny] = true;
+                dfs(depth+1, x, y, sum + map[nx][ny]);
+                visited[nx][ny] = false;
+                visited[x][y] = true;
+            }
+
+            dfs(depth+1, nx, ny, sum + map[nx][ny]);
+        }
+
+        visited[x][y] = false;
+    }
+
+    public static boolean isRange(int x, int y) {
+        return x>=0 && y>=0 && x<n && y<m;
+    }
+}
