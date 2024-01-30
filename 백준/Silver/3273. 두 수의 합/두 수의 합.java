189c3d9e9e987c79481536f82b70845476da40cc
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2024-01-30 16:14:41 +0900

    [Silver III] Title: 두 수의 합, Time: 292 ms, Memory: 27164 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,39 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] num = new int[N];
-        for(int i = 0; i < N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int x = Integer.parseInt(br.readLine());
+        
+        int K = Integer.parseInt(br.readLine());
+        
+        Arrays.sort(arr);
 
-        Arrays.sort(num);
-        int left = 0;
-        int right = N - 1;
-        int result = 0;
-        while (true) {
-            int sum = num[left] + num[right];
-            if(left >= right) {
-                break;
-            } else if(sum > x) {
-                right--;
-            } else if(sum < x) {
-                left++;
+        int answer = 0;
+        int start = 0, end = N-1;
+        while(start < end) {
+            int num = arr[start] + arr[end];
+            if(num == K) {
+                answer++;
+                start++;
+                end--;
+            } else if(num < K) {
+                start++;
             } else {
-                result++;
-                left++;
-                right--;
+                end--;
             }
         }
-        System.out.println(result);
-
+        
+        System.out.println(answer);
 
     }
 }
