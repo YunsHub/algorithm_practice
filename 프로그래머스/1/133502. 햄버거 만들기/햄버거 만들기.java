a517ef697685f9d33bb0d3e8391fe66d0ddada18
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2024-02-27 10:58:04 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.15 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {    
+        // 스택 활용
+        // stack.get() 사용
+    public int solution(int[] ingredient) {
+        int answer = 0;            
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<ingredient.length; i++) {        
+            int food = ingredient[i];
+            stack.add(food);
+            
+            if(stack.size() >= 4 && food == 1) {
+                int size = stack.size()-1;
+                if(stack.get(size-1)==3&&stack.get(size-2)==2&&stack.get(size-3)==1) {
+                    answer++;
+                    for(int j=0; j<4; j++) {
+                        stack.pop();
+                    }
+                } 
+                
+            } 
+        }
+        
+        return answer;
+    }
+}
