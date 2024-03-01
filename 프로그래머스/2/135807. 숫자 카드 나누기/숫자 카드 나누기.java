602febda4d296c650dd5a93cb4500b559bc2f0dd
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2024-03-01 16:27:45 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.68 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        
+        int a = getNum(arrayA, arrayB);
+        int b = getNum(arrayB, arrayA);
+        
+        return Math.max(a, b);
+    }
+    
+    public int getNum(int[] arrayA, int[] arrayB) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=arrayA[0]; i>=2; i--) {
+            boolean check = true;
+            
+            for(int n : arrayA) {
+                if(n % i != 0) {
+                    check = false;
+                    break;    
+                }
+            }
+            if(check) list.add(i);
+        }
+        
+        return checkNum(list, arrayB);
+    }
+    
+    public int checkNum(List<Integer> list, int[] arrayB) {
+        
+        for(int n : list) {
+            System.out.println(n);
+            boolean check = true;
+            for(int i=0; i<arrayB.length; i++) {
+                if(arrayB[i] % n == 0) {
+                    check = false;
+                    break;
+                }
+            }  
+            
+            if(check) {
+                return n;
+            }
+        }
+        
+        return 0;
+    }
+    
+    
+}
