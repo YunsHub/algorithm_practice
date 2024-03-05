c9c67ae4479321b59cb1babe4322a5a097f39cf0
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2024-03-05 23:32:48 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -4,25 +4,41 @@ class Solution {
     // 스택 활용
     // stack.get() 사용
 
-    // 배열 활용
+    // ArrayList 활용
+    
+    // Array 활용    
     public int solution(int[] ingredient) {
         int answer = 0;            
-        Stack<Integer> stack = new Stack<>();
+//         Stack<Integer> stack = new Stack<>();
         
-        for(int i=0; i<ingredient.length; i++) {        
-            int food = ingredient[i];
-            stack.add(food);
+//         for(int i=0; i<ingredient.length; i++) {        
+//             int food = ingredient[i];
+//             stack.add(food);
             
-            if(stack.size() >= 4 && food == 1) {
-                int size = stack.size()-1;
-                if(stack.get(size-1)==3&&stack.get(size-2)==2&&stack.get(size-3)==1) {
-                    answer++;
-                    for(int j=0; j<4; j++) {
-                        stack.pop();
-                    }
-                } 
+//             if(stack.size() >= 4 && food == 1) {
+//                 int size = stack.size()-1;
+//                 if(stack.get(size-1)==3&&stack.get(size-2)==2&&stack.get(size-3)==1) {
+//                     answer++;
+//                     for(int j=0; j<4; j++) {
+//                         stack.pop();
+//                     }
+//                 } 
                 
-            } 
+//             } 
+//         }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int food : ingredient) {
+            list.add(food);
+            if(list.size() >= 4 && food == 1) {
+                int size = list.size() - 1;
+                if(list.get(size-1)==3 && list.get(size-2)==2&&list.get(size-3)==1) {
+                    for(int i=0; i<4; i++) {
+                        list.remove(list.size()-1);
+                    }
+                    answer++;
+                }
+            }
         }
         
         return answer;
