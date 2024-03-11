3c04caf13fd78c7740d70b4baaf648bf1c607aff
KimJungYun <115425807+YunsHub@users.noreply.github.com>
2024-03-11 21:09:40 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.49 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,16 +1,21 @@
 import java.util.*;
 class Solution {
+    public ArrayList<int[]> node;
     public String solution(int[] numbers, String hand) {
         // 왼손 : 1 4 7
         // 오른손 : 3 6 9
         // 가운데 : 2 5 8 0
         
-        // 2가지 방법 : BFS, 좌표 저장
+        // 2가지 방법 : 좌표 저장
         
         // 1. 좌표 저장
-        ArrayList<int[]> node = new ArrayList<>();
-        
+        node = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         // init
+        hand = hand.equals("left") ? "L" : "R";
+        int[] leftHand = {3, 0};
+        int[] rightHand = {3, 2};
+        
         node.add(new int[] {3, 1});
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
@@ -18,47 +23,34 @@ class Solution {
             }
         }
         
-        int[] leftHand = new int[2];
-        int[] rightHand = new int[2];
-        leftHand[0] = 3;
-        leftHand[1] = 0;
-        rightHand[0] = 3;
-        rightHand[1] = 2;
-        
-        
-        StringBuilder sb = new StringBuilder();
-        for(int number : numbers) {
+        for(int number : numbers) {            
+            String nowHand = getHand(hand, number, leftHand, rightHand);
+            sb.append(nowHand);
             
-            if(number == 1 || number == 4 || number == 7) {
-                sb.append("L");
-                leftHand = node.get(number);
-            } else if(number == 3 || number == 6 || number == 9) {
-                sb.append("R");
-                rightHand = node.get(number);
-            } else {
-                int leftDis = getDistance(leftHand, node.get(number));
-                int rightDis = getDistance(rightHand, node.get(number));     
-                
-                if(leftDis < rightDis) {
-                    sb.append("L");
-                    leftHand = node.get(number);
-                } else if(leftDis > rightDis) {
-                    sb.append("R");
-                    rightHand = node.get(number);
-                } else {
-                    if(hand.equals("left")) {
-                        sb.append("L");
-                        leftHand = node.get(number);
-                    } else {
-                        sb.append("R");
-                        rightHand = node.get(number);
-                    }
-                }                                                
-            }
-        }
-        
+            if(nowHand.equals("L")) leftHand = node.get(number);
+            else rightHand = node.get(number);
+        }        
         
         return sb.toString();
+    }
+    
+    public String getHand(String handed, int number, int[] leftHand, int[] rightHand) {
+        if(number == 1 || number == 4 || number == 7) {
+            return "L";
+        } else if(number == 3 || number == 6 || number == 9) {
+            return "R";  
+        } 
+        
+        int leftDis = getDistance(leftHand, node.get(number));
+        int rightDis = getDistance(rightHand, node.get(number));     
+
+        if(leftDis < rightDis) {
+            return "L";
+        } else if(leftDis > rightDis) {
+            return "R";
+        }             
+        
+        return handed;
     }
     
     public int getDistance(int[] now, int[] next) {
